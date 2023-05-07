54202bf3bdb6b3684b0fca7301960fb04da55d79
RayCurse <38709018+RayCurse@users.noreply.github.com>
2023-05-07 13:59:45 -0400

    Integrate back end functionality and endpoints

--- public/scripts.js
+++ public/scripts.js
@@ -7,6 +7,7 @@ window.addEventListener("DOMContentLoaded", function() {
     const gameTypeButtons = document.getElementsByName("selected_game")
     const playOpponentButton = document.getElementById("opponent")
     const playOptionsMenu = document.getElementById("play_options_menu")
+    const results = document.getElementById("results")
 
     // Update drop down options
     for (const button of gameTypeButtons) {
@@ -41,11 +42,40 @@ window.addEventListener("DOMContentLoaded", function() {
             }
         })
     }
-
     form.addEventListener("reset", function(){
         playOptionsMenu.style.visibility = "hidden"
+        results.style.visibility = "hidden"
         for (label of document.querySelectorAll("label[for='play_options_menu']")) {
             label.style.visibility = "hidden"
+        }
+    })
+
+    // Submit form
+    form.addEventListener("submit", function(e){
+        e.preventDefault()
+        const formData = new FormData(form)
+        if (formData.get("game_mode") == "draw") {
+            fetch(`app/${formData.get("selected_game")}/play`)
+                .then((response) => response.json())
+                .then((data) => {
+                    results.innerHTML = `Your random draw is ${data.player}.`
+                    results.style.visibility = "visible"
+                }
+            )
+        } else if (formData.get("game_mode") == "opponent") {
+            fetch(`app/${formData.get("selected_game")}/play`, {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: `{"shot": "${formData.get("play")}"}`,
+            })
+                .then((response) => response.json())
+                .then((data) => {
+                    results.innerHTML = `Opponent played ${data.opponent}. You ${data.result == "win" ? "won" : "lost"}!`
+                    results.style.visibility = "visible"
+                }
+            )
         }
     })
 })
